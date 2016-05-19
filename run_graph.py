5aefbfc530ed8d37274613f8e69862215eda4243
Brian Dorsey <bdor528@gmail.com>
2016-05-18 23:01:13 -0400

    Last Update
    I’m trapped in a foreign place known as the American South, surrounded
    by lifted trucks and racist plantation owners. I failed to pack my
    Macbook charger and my battery is on it’s last leg. These are my final
    changes to my graph theory project. I hope to escape this horrible
    place and return North soon.

--- run_graph.py
+++ run_graph.py
@@ -1,12 +1,17 @@
+import sys
 from graph import Graph
 
-v = ['A', 'B', 'C', 'D']
-e = [('A','B'),('B','C'),('A','D')]
+def main(argv = None):
+	v = ['A', 'B', 'C', 'D']
+	e = [('A','B'),('B','C'),('A','D')]
 
-G = Graph(v,e)
+	G = Graph(v,e)
 
-#G.printNodes()
-#G.printVertices()
-#G.printEdges()
-#print G.getNode('Q').getAdjacent()
-G.spanningTree('A')
+	#G.printNodes()
+	#G.printVertices()
+	#G.printEdges()
+	print G.getNode('A').getAdjacent()
+	#G.spanningTree('A')s
+
+if __name__ == "__main__":
+    sys.exit(main())
